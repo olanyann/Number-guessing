4fbb59578350d26caa3fee6f36794ed1d704215f
Bryan <hirlebryan@gmail.com>
2025-05-29 21:29:57 -0300

    improvements on the GameLayer Class and finishing the logic part of the project on the Main Class

--- src/main/java/numerosaleatorios/Main.java
+++ src/main/java/numerosaleatorios/Main.java
@@ -1,7 +1,37 @@
 package numerosaleatorios;
 
+import numerosaleatorios.integracao.GameLayer;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        GameLayer gameLayer = new GameLayer();
+        Scanner input = new Scanner(System.in);
 
+        while (gameLayer.hasAttempt()) {
+            System.out.println("Enter your guess:");
+            int playerNumber = input.nextInt();
+            gameLayer.setGuessedNumber(playerNumber);
+
+            if (gameLayer.isCorrect()) {
+                System.out.println("Congratulations! You guessed the correct number!");
+                break;
+            } else {
+                gameLayer.registerAttempt();
+                System.out.println("Wrong guess! You have " + gameLayer.remainingAttempts() + " attempts left.");
+                if (!gameLayer.isHot()) {
+                    System.out.println("The target number is higher!");
+
+                } else {
+                    System.out.println("The target number is lower!");
+                }
+            }
+        }
+
+        if (!gameLayer.isCorrect()) {
+            System.out.println("Game over! You've run out of attempts. the number was " + gameLayer.getKeyNumber() );
+        }
+
+        }
     }
-}
--- src/main/java/numerosaleatorios/integracao/GameLayer.java
+++ src/main/java/numerosaleatorios/integracao/GameLayer.java
@@ -3,25 +3,30 @@ package numerosaleatorios.integracao;
 import numerosaleatorios.logic.AttemptManager;
 import numerosaleatorios.logic.Randomizer;
 
+
 public class GameLayer {
     private final Randomizer randomizer = new Randomizer();
     private final AttemptManager attemptManager = new AttemptManager(5);
+
     private final int keyNumber = randomizer.generateRandomNumber();
 
     int guessedNumber;
 
 
 
+    public int getKeyNumber() {
+        return keyNumber;
+    }
 
     public void setGuessedNumber(int guessedNumber) {
         this.guessedNumber = guessedNumber;
     }
 
     public boolean isCorrect() {
-        if (guessedNumber == keyNumber) {
-            return true;
-        }
-        return false;
+       return this.guessedNumber == keyNumber;
+    }
+    public boolean isHot(){
+        return this.guessedNumber > keyNumber;
     }
     public void registerAttempt() {
         attemptManager.registerTry();
